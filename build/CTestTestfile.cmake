3492acfdeb1a0850f00dc800de5c1259d3dab5ec
Dawei Li <dawei.li@bullish.com>
2022-01-24 17:26:51 -0500

    delete build files since they are not for code review

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dawei.li/Work/kata_accounting
-# Build directory: /Users/dawei.li/Work/kata_accounting/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dawei.li/Work/kata_accounting/tests
-# Build directory: /Users/dawei.li/Work/kata_accounting/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(accounting_tests "/Users/dawei.li/Work/kata_accounting/build/tests/accounting_tests" "--report_level=detailed" "--color_output")
-set_tests_properties(accounting_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/dawei.li/Work/eos/build/lib/cmake/eosio/EosioTester.cmake;143;add_test;/Users/dawei.li/Work/kata_accounting/tests/CMakeLists.txt;20;add_eosio_test;/Users/dawei.li/Work/kata_accounting/tests/CMakeLists.txt;0;")
